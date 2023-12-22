adc7399a4311736b0352b675693cd81dbeab524f
ajmal-j <ajmaljaleel7860@gmail.com>
2023-12-23 00:25:38 +0530

    Added Linkdin link

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,9 +1,12 @@
 const Footer = () => {
   return (
-    <div className='text-gray-500 fixed left-0 right-0 bottom-0 ps-3 pb-3'>
+    <a
+      href='https://www.linkedin.com/in/ajmal-jaleel-1267b9275?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
+      className='text-gray-500 bg-black fixed left-0 px-2 bottom-0 ms-4 mb-2  rounded-full'
+    >
       <i className=' fa-brands fa-linkedin'></i>{" "}
       <span className='text-sm'>| ajmal jaleel</span>
-    </div>
+    </a>
   );
 };
 
