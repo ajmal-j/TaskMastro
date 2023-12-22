15d05ba88bab513213450f8969691aed30d3bad4
ajmal-j <ajmaljaleel7860@gmail.com>
2023-12-23 00:18:08 +0530

    Footer correction

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className='text-gray-500 fixed bg-black shadow-offset-top shadow-black left-0 right-0 bottom-0 text-center pb-3'>
+    <div className='text-gray-500 fixed left-0 right-0 bottom-0 ps-3 pb-3'>
       <i className=' fa-brands fa-linkedin'></i>{" "}
       <span className='text-sm'>| ajmal jaleel</span>
     </div>
