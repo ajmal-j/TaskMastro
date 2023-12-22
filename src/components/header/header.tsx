54412cde72200e8336d3b5d6f78d861872e4caf4
ajmal-j <ajmaljaleel7860@gmail.com>
2023-12-23 00:12:49 +0530

    correction: time and footer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,33 +4,32 @@ import { useEffect, useState } from "react";
 
 const Header = () => {
   const [time, setTime] = useState<Date>(new Date());
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTime(new Date());
-    }, 6000);
+    }, 5000);
     return () => clearInterval(intervalId);
   }, []);
-  let hours: number = time.getHours();
-  let am: boolean = true;
 
-  if (hours >= 12) {
-    am = false;
-    if (hours > 12) {
-      hours -= 12;
-    }
-  }
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
 
-  let formattedHours: string = hours < 10 ? "0" + hours : "" + hours;
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
 
-  let formattedTime: string = formattedHours + (am ? " am" : " pm");
+  let strHours = hours < 10 ? "0" + hours : hours.toString();
+  let strMinutes = minutes < 10 ? "0" + minutes : minutes.toString();
 
   return (
     <div className='flex items-center p-2 pt-4 mb-5 '>
       <img className='w-[55px]' src='/logo.svg' alt='logo' />
       <span className='text-xl font-thin text-white'>To Do App</span>
       <p className='timeDiv'>
-        <span className='hour'>{formattedHours}</span>
-        <span className='minutes'>{formattedTime}</span>
+        <span className='hour'>{strHours}</span>
+        <span className='minutes'>{strMinutes}</span>
+        {" " + ampm}
       </p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "offset-top":
+          "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px #00000",
+      },
+    },
   },
   plugins: [],
-}
+};
